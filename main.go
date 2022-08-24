b6a765793ff5dcf48a72a234c1c0029e4f66687d
Nada Salsabila <99460188+nadahusin@users.noreply.github.com>
2022-08-24 20:46:38 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,13 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	for i := 6; i >= 1; i-- {
-		for k := 1; k <= (i*2)-1; k++ {
+	printSegitiga()
+}
+
+func printSegitiga() {
+
+	var i, j, k, rows int
+
+	fmt.Print("Input Number = ")
+	fmt.Scanln(&rows)
+
+	for i = rows; i >= 1; i-- {
+		for k = 1; k <= (i*2)-1; k++ {
 			fmt.Print("*")
 		}
 		println()
 
-		for j := 6; j >= i; j-- {
+		for j = rows; j >= i; j-- {
 			fmt.Print(" ")
 		}
 	}
