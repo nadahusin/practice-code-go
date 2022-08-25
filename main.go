75121956cbfcc8ff22708a14a35f249dae9f3450
Nada Salsabila <99460188+nadahusin@users.noreply.github.com>
2022-08-25 09:05:25 +0700

    Update main.go
    void function dengan nama “printSegitiga” yg menerima tipe data number sebagai params dan menghasilkan output segitiga terbalik

--- main.go
+++ main.go
@@ -3,23 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	printSegitiga()
+	printSegitiga(5)
 }
 
-func printSegitiga() {
+func printSegitiga(rows int) {
 
-	var i, j, k, rows int
-
-	fmt.Print("Input Number = ")
-	fmt.Scanln(&rows)
-
-	for i = rows; i >= 1; i-- {
-		for k = 1; k <= (i*2)-1; k++ {
+	for i := rows; i >= 1; i-- {
+		for k := 1; k <= (i*2)-1; k++ {
 			fmt.Print("*")
 		}
 		println()
 
-		for j = rows; j >= i; j-- {
+		for j := rows; j >= i; j-- {
 			fmt.Print(" ")
 		}
 	}
